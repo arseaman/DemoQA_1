932425e38d9a0b95a2d86722c54bdd63b4f23b46
Vitaly <arseaman9295@gmail.com>
2023-11-20 23:14:20 +0200

    add yml file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
-
-desiredCapabilities = {
-"browserName":"chrome"
-}
 
 @pytest.fixture()
 def driver():
-    driver = webdriver.Remote(command_executor='http://192.168.0.107:4444/wd/hub', desired_capabilities = desiredCapabilities)
+    options = ChromeOptions()
+    options.browser_version = '119.0.6045.160'
+    options.platform_name = 'Ubuntu'
+    cloud_options = {
+        'build': 'my_test_build',
+        'name': 'my_test_name'
+    }
+    options.add_experimental_option('w3c', True)
+    options.add_experimental_option('cloud:options', cloud_options)
+    cloud_url = 'http://192.168.0.107:4444/wd/hub'
+    driver = webdriver.Remote(cloud_url, options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
