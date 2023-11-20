a9a071a6fc001c9fb9f5967ca67f3b7ab9c032a3
Vitaly <arseaman9295@gmail.com>
2023-11-20 23:59:19 +0200

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,20 +3,34 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
+# @pytest.fixture()
+# def driver():
+#     options = ChromeOptions()
+#     options.browser_version = '119.0.6045.160'
+#     options.platform_name = 'Linux'
+#     cloud_options = {
+#         'build': 'my_test_build',
+#         'name': 'my_test_name'
+#     }
+#     options.add_experimental_option('w3c', True)
+#     options.add_experimental_option('cloud:options', cloud_options)
+#     cloud_url = 'http://54.174.220.59:4444/wd/hub'
+#     driver = webdriver.Remote(cloud_url, options=options)
+#     driver.maximize_window()
+#     yield driver
+#     driver.quit()
 @pytest.fixture()
 def driver():
     options = ChromeOptions()
     options.browser_version = '119.0.6045.160'
-    options.platform_name = 'Ubuntu'
+    options.platform_name = 'Linux'
     cloud_options = {
         'build': 'my_test_build',
         'name': 'my_test_name'
     }
-    options.add_experimental_option('w3c', True)
-    options.add_experimental_option('cloud:options', cloud_options)
     cloud_url = 'http://54.174.220.59:4444/wd/hub'
+    options.set_capability('cloud:options', cloud_options)
     driver = webdriver.Remote(cloud_url, options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
-
