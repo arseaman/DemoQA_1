6a26533f3b0375deeb4340f09c7d4ff054835f8d
Vitaly <arseaman9295@gmail.com>
2023-11-21 17:06:20 +0200

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,7 +28,7 @@ def driver():
         'build': 'my_test_build',
         'name': 'my_test_name'
     }
-    cloud_url = 'http://54.174.220.59:4444/wd/hub'
+    cloud_url = 'http://172.31.42.69:4444/wd/hub'
     options.set_capability('cloud:options', cloud_options)
     driver = webdriver.Remote(cloud_url, options=options)
     driver.maximize_window()
